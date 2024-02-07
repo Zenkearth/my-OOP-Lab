5f3b77a2524f305a209450fca0ac70860f555a7c
Zenkearth <Peeraphatnaowasaisee1@gmail.com>
2024-02-07 16:48:10 +0700

    Update print position

--- Board Game Pre Chess/Board.java
+++ Board Game Pre Chess/Board.java
@@ -7,19 +7,32 @@ public class Board {
     public Board() {
         playerFigures[0] = new Figure("Player 1", 0);
         playerFigures[1] = new Figure("Player 2", 5);
-        for (Figure playFigure : playerFigures){
+        for (Figure playFigure : playerFigures) {
             positionBuffer[playFigure.getNowPosition()] = 1;
         }
     }
 
     public void displayBoard() {
         for (Figure figure : playerFigures) {
-            System.out.println(figure.getName() + " is at position: " + figure.getNowPosition());
+            System.out.println(figure.getName() + " is at position: " + (figure.getNowPosition() + 1));
         }
-    }   
-     public void moveFigure(int playerIndex,int movePosition){
-        if(movePosition >= 1 && movePosition <= tableSize){
+    }
+
+    public void moveFigure(int playerIndex, int movePosition) {
+        movePosition--;
+        if (movePosition >= 1 && movePosition <= tableSize) {
+            positionBuffer[playerFigures[playerIndex].getNowPosition()] = 0;
             playerFigures[playerIndex].setNowPosition(movePosition);
+            positionBuffer[movePosition] = 1;
+        } else {
+            System.out.println("Invalid move. Please choose a position within the board limits.");
         }
-     }
+    }    
+
+    public void printPositionBuffer() {
+        for (int i : positionBuffer) {
+            System.out.printf("%d ", i);
+        }
+        System.out.println();
+    }
 }
--- Board Game Pre Chess/Player.java
+++ Board Game Pre Chess/Player.java
@@ -1,9 +1,21 @@
 public class Player {
     public static void main(String[] args) {
-        Board game = new Board();
-        game.displayBoard();
-        game.moveFigure(0, 3);
-        System.out.println();
-        game.displayBoard();
+        Board game = new Board();// create Board
+
+        game.displayBoard(); //view position
+        game.printPositionBuffer(); // display
+
+        game.moveFigure(0, 3); // move player 1 from 1 to 3
+
+        game.displayBoard(); //view position
+        game.printPositionBuffer(); //display
+        
+        game.moveFigure(1, 2); // move player 2 from 5 to 2
+        game.displayBoard(); //view position
+        game.printPositionBuffer(); //display
+        
+        game.moveFigure(1, 10); // move player 2 from 2 to 8
+        game.displayBoard(); //view position
+        game.printPositionBuffer(); //display
     }   
 }
