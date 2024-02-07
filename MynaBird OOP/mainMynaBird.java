f9ca49699ce868ea52d8eacb9b78f716d29a28e5
Zenkearth <Peeraphatnaowasaisee1@gmail.com>
2024-02-07 09:45:28 +0700

    add test case 'have sex'

--- MynaBird OOP/mainMynaBird.java
+++ MynaBird OOP/mainMynaBird.java
@@ -7,46 +7,56 @@ public class mainMynaBird {
             // Test Case 1: Print properties
             littlebird.getProp();
             System.out.println("------------------------------------------------------------------");
-            
+
             // Test Case 2: Default talk
             littlebird.talk();
             System.out.println("------------------------------------------------------------------");
-            
+
             // Test Case 3: Custom talk
             littlebird.talk("Hello, I am a Myna Bird!");
             System.out.println("------------------------------------------------------------------");
-            
+
             // Test Case 4: Eating (normal)
             littlebird.eat(5); // Eat 5 units of food
             System.out.println("------------------------------------------------------------------");
-            
+
             // Test Case 5: Overeating
             littlebird.eat(100); // Attempt to overeat
             System.out.println("------------------------------------------------------------------");
-            
+
             // Test Case 6: Pooping
             littlebird.poop(3); // Poop 3 units
             System.out.println("------------------------------------------------------------------");
-            
+
             // Test Case 7: Switching Bird Type
             littlebird.swBirdType();
             System.out.println("------------------------------------------------------------------");
-            
+
             // Test Case 8: Another poop after switching type
             littlebird.eat(30);
             littlebird.poop(20);
             System.out.println("------------------------------------------------------------------");
-            
+
             // Test Case 9: Check properties again
             littlebird.getProp();
             System.out.println("------------------------------------------------------------------");
-            
+
             // Test Case 10: have sex
             MynaBird girlFrieBird = new MynaBird("Lisa", 4, 30, true, false);
             MynaBird baby = girlFrieBird.haveSex(littlebird);
             baby.getProp();
             System.out.println("------------------------------------------------------------------");
 
+            // Test Case 10: have sex
+            MynaBird boyFrieBird = new MynaBird("Tom", 4, 30, true, true);
+            MynaBird birddd = littlebird.haveSex(boyFrieBird);
+            if (birddd == null) {
+                System.out.println("The baby bird never came to be.");
+            } else {
+                birddd.getProp();
+            }
+
+            System.out.println("------------------------------------------------------------------");
         } catch (IllegalArgumentException e) {
             System.out.println(e.getMessage());
         }
