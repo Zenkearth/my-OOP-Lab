631db430539545ad883489de5c3a2d5b643684df
Zenkearth <Peeraphatnaowasaisee1@gmail.com>
2024-02-22 18:41:09 +0700

    Update abstrect method

--- Board Game Pre Chess/Figure.java
+++ Board Game Pre Chess/Figure.java
@@ -1,29 +1,49 @@
-public class Figure {
+public abstract class Figure {
+
     private String name;
-    private int nowPosition;
+    private boolean isWhite;
+    private int row;
+    private int column;
 
-    public Figure(String figureName, int position) {
-        this.name = figureName;
-        this.nowPosition = position;
-
+    public Figure(String name, boolean isWhite, int row, int column) {
+        this.name = name;
+        this.isWhite = isWhite;
+        this.row = row;
+        this.column = column;
     }
 
-    // ------------------------------------------------------------
     public String getName() {
         return name;
     }
 
-    public int getNowPosition() {
-        return nowPosition;
+    public boolean isWhite() {
+        return isWhite;
     }
 
-    public void setName(String name) {
-        this.name = name;
+    public int getRow() {
+        return row;
     }
 
-    public void setNowPosition(int nowPosition) {
-        this.nowPosition = nowPosition;
+    public int getColumn() {
+        return column;
     }
 
-    // ------------------------------------------------------------
+    public void setPosition(int row, int column) {
+        this.row = row;
+        this.column = column;
+    }
+
+    protected boolean isSameColor(Figure other) {
+        return this.isWhite == other.isWhite;
+    }
+
+    protected boolean isOnBoard(int row, int column) {
+        return 0 <= row && row < 8 && 0 <= column && column < 8;
+    }
+
+    protected boolean isOccupied(ChessBoard board, int row, int column) {
+        return board.getFigureAt(row, column) != null;
+    }
+
+    public abstract boolean freeMove(int destinationRow, int destinationColumn, ChessBoard board);
 }
